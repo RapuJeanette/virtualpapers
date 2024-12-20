54347c1afe7f2dce29cb64c370bba7aec137a29d
RapuJeanette <rapupena2909@gmail.com>
2024-12-20 14:32:02 -0400

    "h"

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
             },
         }),
     ],
-    base: 'http://mail.tecnoweb.org.bo/inf513/grupo07sa/proyecto2/public/',
+    baseUrl: 'http://mail.tecnoweb.org.bo/inf513/grupo07sa/proyecto2/public/',
     resolve: {
         alias: {
           '@': resolve(__dirname, 'resources/js'),
