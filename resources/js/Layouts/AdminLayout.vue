fd4125330240854eb1f3f8fcd76fe084aeaa1058
RapuJeanette <rapupena2909@gmail.com>
2024-12-21 13:44:12 -0400

    "1.3"

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -9,14 +9,14 @@
         <aside class="w-64 p-4 text-white bg-gray-800">
           <nav>
             <ul>
-              <li><a href="/admin/ventas" class="block px-4 py-2 hover:bg-gray-700">Ventas</a></li>
-              <li><a href="../admin/usuarios" class="block px-4 py-2 hover:bg-gray-700">Usuarios</a></li>
-              <li><a href="../admin/categorias" class="block px-4 py-2 hover:bg-gray-700">Categoria</a></li>
-              <li><a href="../admin/pagos" class="block px-4 py-2 hover:bg-gray-700">Pagos</a></li>
-              <li><a href="../admin/productos" class="block px-4 py-2 hover:bg-gray-700">Productos</a></li>
-              <li><a href="../admin/promocion" class="block px-4 py-2 hover:bg-gray-700">Promociones</a></li>
-              <li><a href="../admin/inventarios" class="block px-4 py-2 hover:bg-gray-700">Inventario</a></li>
-              <li><a href="../admin/compras" class="block px-4 py-2 hover:bg-gray-700">Compras</a></li>
+              <li><a href="Admin/ventas" class="block px-4 py-2 hover:bg-gray-700">Ventas</a></li>
+              <li><a href="Admin/usuarios" class="block px-4 py-2 hover:bg-gray-700">Usuarios</a></li>
+              <li><a href="Admin/categorias" class="block px-4 py-2 hover:bg-gray-700">Categoria</a></li>
+              <li><a href="Admin/pagos" class="block px-4 py-2 hover:bg-gray-700">Pagos</a></li>
+              <li><a href="Admin/productos" class="block px-4 py-2 hover:bg-gray-700">Productos</a></li>
+              <li><a href="Admin/promocion" class="block px-4 py-2 hover:bg-gray-700">Promociones</a></li>
+              <li><a href="Admin/inventarios" class="block px-4 py-2 hover:bg-gray-700">Inventario</a></li>
+              <li><a href="Admin/compras" class="block px-4 py-2 hover:bg-gray-700">Compras</a></li>
             </ul>
           </nav>
         </aside>
